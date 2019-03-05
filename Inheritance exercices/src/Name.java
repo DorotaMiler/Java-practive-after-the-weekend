b694ce2bcc673349317cf5138eab3268ef5c387f
Dorota Miler <dorota.miler@outlook.com>
2019-03-05 22:40:47 +0100

    creation of simple Name.java resulting in Hello + name println

---
+++ Inheritance exercices/src/Name.java
@@ -0,0 +1,10 @@
+public class Name {
+
+  private static void sayHello(String name){
+      System.out.println("Hello "+name);
+  }
+
+    public static void main(String[] args) {
+        sayHello("Dorota");
+    }
+}
