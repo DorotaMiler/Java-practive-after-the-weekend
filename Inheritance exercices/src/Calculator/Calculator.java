252a056ce63b9aa1160fdd1209cf505a6fd99c01
Dorota Miler <dorota.miler@outlook.com>
2019-03-06 22:58:54 +0100

    Calculator addition of different types of functions in regards to private variable result; we've got void add(), int add(), int getResult() for comparison

---
+++ Inheritance exercices/src/Calculator/Calculator.java
@@ -0,0 +1,20 @@
+package Calculator;
+
+public class Calculator {
+
+    private int result = 0;
+
+    public void add(int v){
+        return v;
+    }
+
+    public int add (int a, int b){
+        return a+b;
+    }
+
+    public int getResult(){
+        return result;
+    }
+
+
+}
